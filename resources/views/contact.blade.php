03519adbde8033cf8ba9c9b84a38f2df52576c35
maleksohil <42293783+maleksohil@users.noreply.github.com>
2024-10-16 15:04:43 +0530

    done changes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,6 +12,9 @@
         
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required><br><br>
+
+        <label for="age">Age:</label>
+        <input type="text" id="age" name="age" required><br><br>
         
         <button type="submit">Submit</button>
     </form>

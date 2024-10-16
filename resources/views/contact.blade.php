a9f837f14d73c1ad748d77e143947211607fe9bd
maleksohil <42293783+maleksohil@users.noreply.github.com>
2024-10-16 16:06:44 +0530

    done changes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,13 +7,12 @@
     <h1>Contact Us</h1>
     <form method="POST" action="/contact">
         @csrf
-        <label for="name">Name:</label>
+        <label for="name">Please Enter Name:</label>
         <input type="text" id="name" name="name" required><br><br>
         
-        <label for="email">Email:</label>
+        <label for="email">Please Enter Email:</label>
         <input type="email" id="email" name="email" required><br><br>
 
-        
         <button type="submit">Submit</button>
     </form>
 </body>
